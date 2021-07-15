09c54c9be17603256ab8d0320f70b802d77a00ad
fox-io <gitlab.fox-io@foxdata.io>
2021-07-15 08:32:57 -0500

    Using list of names, save copies of the letter to the output folder.

--- main.py
+++ main.py
@@ -11,11 +11,26 @@ uniquely-named files containing a personalized version of the letter.
 
 
 def main():
+    NAME_TEMPLATE = "[name]"
+
+    # Read in a copy of our letter.
     with open("./input/letter.txt") as letter_file:
         letter_contents = letter_file.read()
 
+    # Read in the contents of our names.txt file.
     with open("./input/names.txt") as names_file:
         names_contents = names_file.read()
+
+        # Convert the contents to a list of names.
+        name_list = names_contents.split("\n")
+
+        for name in name_list:
+            # Replace our template tag with each name.
+            letter_output = letter_contents.replace(NAME_TEMPLATE, name)
+
+            # Save each file individually named.
+            with open(f"./output/letter-{name}", "w") as output_file:
+                output_file.write(letter_output)
 
 
 if __name__ == "__main__":
