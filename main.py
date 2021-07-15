f7fc1ffaece082157c60b5d54957214b9f262304
fox-io <gitlab.fox-io@foxdata.io>
2021-07-14 21:36:37 -0500

    Added main.py stub

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+py-mail-merge/main.py
+
+Mail Merge
+
+Using a provided sample letter and names, this script will generate a series of
+uniquely-named files containing a personalized version of the letter.
+
+(c)2021 John Mann <github.fox-io@foxdata.io>
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
